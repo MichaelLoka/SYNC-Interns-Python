2aae28a0a570d8836a145959d3705dc5b77175e4
MichaelLoka <michael.loka.2001@gmail.com>
2023-10-24 16:22:59 +0300

    Handeled URL Shortener Libs

--- Task 3 (URL Shortener)/URL_Shortener.py
+++ Task 3 (URL Shortener)/URL_Shortener.py
@@ -1,9 +1,12 @@
 import pyshorteners as shr
 
+# Take the URL From the User
 url = input("Please Enter The URL: ")
 
+# Function Uses Pyshortener Library For URL Shortening
 def shorten_link(url):
     short = shr.Shortener()
     print(short.tinyurl.short(url))
 
+# Calling the Function with the Intended URL
 shorten_link(url)
